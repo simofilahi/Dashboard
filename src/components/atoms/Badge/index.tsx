d8efe8d959968f44b2bf78f1c21174836a479f3a
simofilahi <mohamedfilahi222@gmail.com>
2022-09-19 13:58:35 +0100

    wip

---
+++ src/components/atoms/Badge/index.tsx
@@ -0,0 +1,11 @@
+import Container from "./index.styled";
+
+interface BadgeProps {
+  color?: string;
+}
+
+const Badge = () => {
+  return <Container color=""></Container>;
+};
+
+export default Badge;
